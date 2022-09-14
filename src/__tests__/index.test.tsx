90f6bcdaaab97f551f087f0e7cc4e67ee8c78e3c
Daniele Di Sarli <danieleds0@gmail.com>
2022-09-14 16:47:31 +0200

    test: add initial test

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,1 +1,27 @@
-it.todo('write a test');
+import React from 'react';
+import { render } from '@testing-library/react-native';
+import fetchMock from 'fetch-mock'
+import RoomPreview from '../components/RoomPreview'
+
+describe('Makes the appropriate network requests', () => {
+
+    describe("", () => {
+        beforeEach(() => {
+            fetchMock
+                .mock('http://example.com/loading', 200)
+                .mock('http://example.com/preview', 200)
+        });
+
+        afterEach(() => {
+            fetchMock.restore();
+        });
+
+        it('Fetches preview on first render if the preview is ready', () => {
+            render(<RoomPreview loadingUrl={{ uri: 'http://example.com/loading' }} previewUrl={{ uri: 'http://example.com/preview' }} />)
+
+            expect(fetchMock.called('http://example.com/loading', { method: 'HEAD' })).toBe(true)
+            expect(fetchMock.called('http://example.com/preview', { method: 'HEAD' })).toBe(true)
+        })
+    })
+    
+})
